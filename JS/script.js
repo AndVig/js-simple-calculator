26b9ebf871b8f9e045d40dfa14b60a4bfec7616c
AndVig <andrea.vigna2@gmail.com>
2024-05-10 11:52:27 +0200

    m3 and 4 complete

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,12 @@
 //buttons
 btnNum =document.getElementsByClassName("btnNum");
-btnResult=document.getElementById("btnResult");
+const btnResult=document.getElementById("btnResult");
 const display=document.getElementById("display");
 btnCanc=document.getElementById("btnCanc");
 //variables
 var inputOne=0;
 var inputTwo=0;
+let sum=0;
 var isFirst=true;
 var op="";
 // btnNum.addEventListener('click',addNum(btnNum))
@@ -48,4 +49,33 @@ function canc(){
     isFirst=true;
     op="";
     console.log("input= "+inputOne+" 1st "+isFirst+" operazione: "+op);
+}
+
+btnResult.addEventListener('click',result());
+
+function result(){
+    if(isFirst){
+        display.value="Inserire operatore prima, ricomincia da capo"
+    }else{
+        inputTwo=parseInt(display.value);
+        switch(op){
+            case 'plus':
+                sum=inputOne+inputTwo;
+                break;
+            case 'min':
+                sum=inputOne-inputTwo;
+                display.value=sum;
+                break;
+            case 'per':
+                sum=inputOne*inputTwo;
+                display.value=sum;
+                break;
+            case 'div':
+                sum=inputOne/inputTwo;
+                display.value=sum;
+                break;
+        }
+
+    }
+
 }
